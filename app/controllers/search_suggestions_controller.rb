d367c821487a9488f1007dab0ba1894f2a29eb04
moeamaya <moeamaya@gmail.com>
2013-01-20 13:14:12 -0500

    postgres query

--- app/controllers/search_suggestions_controller.rb
+++ app/controllers/search_suggestions_controller.rb
@@ -1,7 +1,13 @@
 class SearchSuggestionsController < ApplicationController
   
   def index
-    @city = City.search(params[:term])
+    search = params[:term]
+    if search
+      @city = City.where( ["name LIKE ?", "%#{search}%"] )
+    else
+      @city = City.all
+    end
+    
     render :json => @city.map { |city| city.name }
   
   end
