6ae2de6239a3b116524085bcc4cbdcc2e84a12f6
amdixon <alexander.m.dixon@gmail.com>
2013-01-29 15:12:03 -0500

    random search params

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -1,7 +1,15 @@
 class CityController < ApplicationController
 
   def index
-    @city = City.search(params[:search]).take(1)
+    test = params[:search]
+    
+    if test != ''
+      search = test.upcase
+      @city = City.search(params[:search]).take(1)
+    else
+      @city = City.find(:all, :order => 'RANDOM ()').take(1)
+    end
+    
     if signed_in?
       @profile = current_user.profile
       @favorites = Favorite.where("profile_id = ?", @profile.user_id).limit(10)
